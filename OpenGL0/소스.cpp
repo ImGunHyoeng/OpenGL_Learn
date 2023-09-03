d0b59cdb1ec29191cef0ae82ec1c77373877499c
ImGunHyoeng <ynoe30@gmail.com>
2023-09-03 23:36:23 +0900

    pratice triangle to graphic pipeline

--- OpenGL0/소스.cpp
+++ OpenGL0/소스.cpp
@@ -1,66 +1,66 @@
-#include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-// DESC :> window ≈©±‚ ∫Ø∞Ê Ω√∏∂¥Ÿ callback «‘ºˆ »£√‚
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-
-// DESC :> Input ¿Ã∫•∆Æ
-void processInput(GLFWwindow* window);
-
-int main()
-{
-	// DESC :> glfw initialize
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(800, 500, "LearnOpenGL", NULL, NULL);
-
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-
-	// DESC :> glad initialize
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	// DESC :> rendering infinity loop
-	while (!glfwWindowShouldClose(window))
-	{
-		processInput(window);
-
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	// DESC :> «“¥Áµ» ∏µÁ ∏ﬁ∏∏Æ ªË¡¶
-	glfwTerminate();
-
-	return 0;
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-}
+//#include <iostream>
+//#include <glad/glad.h>
+//#include <GLFW/glfw3.h>
+//
+//// DESC :> window ÌÅ¨Í∏∞ Î≥ÄÍ≤Ω ÏãúÎßàÎã§ callback Ìï®Ïàò Ìò∏Ï∂ú
+//void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+//
+//// DESC :> Input Ïù¥Î≤§Ìä∏
+//void processInput(GLFWwindow* window);
+//
+//int main()
+//{
+//	// DESC :> glfw initialize
+//	glfwInit();
+//	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//
+//	GLFWwindow* window = glfwCreateWindow(800, 500, "LearnOpenGL", NULL, NULL);
+//
+//	if (window == NULL)
+//	{
+//		std::cout << "Failed to create GLFW window" << std::endl;
+//		glfwTerminate();
+//
+//		return -1;
+//	}
+//	glfwMakeContextCurrent(window);
+//
+//	// DESC :> glad initialize
+//	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+//	{
+//		std::cout << "Failed to initialize GLAD" << std::endl;
+//		return -1;
+//	}
+//
+//	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+//
+//	// DESC :> rendering infinity loop
+//	while (!glfwWindowShouldClose(window))
+//	{
+//		processInput(window);
+//
+//		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+//		glClear(GL_COLOR_BUFFER_BIT);
+//
+//		glfwSwapBuffers(window);
+//		glfwPollEvents();
+//	}
+//
+//	// DESC :> Ìï†ÎãπÎêú Î™®Îì† Î©îÎ™®Î¶¨ ÏÇ≠Ï†ú
+//	glfwTerminate();
+//
+//	return 0;
+//}
+//
+//void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+//{
+//	glViewport(0, 0, width, height);
+//}
+//
+//void processInput(GLFWwindow* window)
+//{
+//	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+//		glfwSetWindowShouldClose(window, true);
+//}
